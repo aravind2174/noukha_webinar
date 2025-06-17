7abbd9393af09d8c4112e7e9ba90259637d71187
aravind2174 <aravindkannan@noukha.in>
2025-06-17 19:11:26 +0530

    Update supabaseClient.ts

--- project-bolt-sb1-sde6slkp/project/src/supabaseClient.ts
+++ project-bolt-sb1-sde6slkp/project/src/supabaseClient.ts
@@ -1,6 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://znpdhijgcjkmdmolxvcj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpucGRoaWpnY2prbWRtb2x4dmNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0NTk0MjcsImV4cCI6MjA2NTAzNTQyN30.ABO4LmKGxh-XpC1IUpP6atQie5s5CpN5eo80TnW3JnM';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
